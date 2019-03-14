35f749880f7f3094245adfa942dea813f42debf6
Felipe Bersani <bersanifelipe@gmail.com>
2019-03-14 10:14:13 -0300

    Teste

---
+++ src/main/java/com/example/apiRestfull/ProjetoEstudoApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.apiRestfull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoEstudoApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoEstudoApiRestApplication.class, args);
+	}
+
+}
