2828b998830a239fc2775b078da02801f8b402be
Felipe Bersani <bersanifelipe@gmail.com>
2019-03-14 10:17:52 -0300

    Teste2

--- src/main/java/com/example/apiRestfull/ProjetoEstudoApiRestApplication.java
+++ src/main/java/com/example/apiRestfull/ProjetoEstudoApiRestApplication.java
@@ -8,6 +8,7 @@ public class ProjetoEstudoApiRestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetoEstudoApiRestApplication.class, args);
+		System.out.println("Teste");
 	}
 
 }
